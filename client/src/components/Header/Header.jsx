f378cd5877fbb13f4607cad1994143c0bddbb2a9
wIceh <153243884+wIceh@users.noreply.github.com>
2025-04-22 21:17:22 +0200

    Update Header.jsx

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -43,7 +43,7 @@ const Header = React.memo(
       <div className={styles.wrapper}>
         {!project && (
           <Link to={Paths.ROOT} className={classNames(styles.logo, styles.title)}>
-            Planka
+            Ice
           </Link>
         )}
         <Menu inverted size="large" className={styles.menu}>
